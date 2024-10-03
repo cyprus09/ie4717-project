b4de55ee1e4d384274a1697dc0670fa8c0de4946
kevinlee3 <kevin.leegunawan8@gmail.com>
2024-10-04 01:28:08 +0800

    reorganise folders, set up default css, added fonts and logo assets

---
+++ pages/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>FootScape</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../assets/logos/favicon-logo.png" type="image/png" />
+    <!-- css -->
+    <link rel="stylesheet" href="../styles/main.css" />
+    <link rel="stylesheet" href="../styles/pages/home.css" />
+  </head>
+  <body>
+    <header class="navbar">
+      Hello World
+    </header>
+  </body>
+</html>
