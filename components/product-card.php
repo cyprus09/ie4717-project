411ce6e836a289c9745d542e407d9138b1c60fff
mayankpallai <mayankpallai@gmail.com>
2024-10-28 23:41:18 +0800

    add css

--- components/product-card.php
+++ components/product-card.php
@@ -1,4 +1,4 @@
-<a class="product-card" href="#">
+<a class="product-card" href="../pages/prod_desc.php">
     <div class="product-img"></div>
     <div class="product-desc-section">
         <h3 class="product-name">Jordan 1</h3>
