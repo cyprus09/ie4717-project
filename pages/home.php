b502137fd0ca1386aa333713ed928489703ca372
kevinlee3 <kevin.leegunawan8@gmail.com>
2024-11-02 04:19:13 +0800

    added catalog links and category card

--- pages/home.php
+++ pages/home.php
@@ -28,19 +28,19 @@
     <div class="highlight-section main-highlight">
       <?php include "../components/main-carousel.php" ?>
       <div class="brand-category-section">
-        <a class="brand-category-item" href="#">
+        <a class="brand-category-item" href="./catalog.php?brand=Nike">
           <img class="brand-logo" src="../assets/icons/brand/nike.svg" alt="">
           <h2 class="brand-name">Nike</h2>
         </a>
-        <a class="brand-category-item" href="#">
+        <a class="brand-category-item" href="./catalog.php?brand=Adidas">
           <img class="brand-logo" src="../assets/icons/brand/adidas.svg" alt="">
           <h2 class="brand-name">Adidas</h2>
         </a>
-        <a class="brand-category-item" href="#">
+        <a class="brand-category-item" href="./catalog.php?brand=Puma">
           <img class="brand-logo" src="../assets/icons/brand/puma.svg" alt="">
           <h2 class="brand-name">Puma</h2>
         </a>
-        <a class="brand-category-item" href="#">
+        <a class="brand-category-item" href="./catalog.php?brand=Converse">
           <img class="brand-logo" src="../assets/icons/brand/converse.svg" alt="">
           <h2 class="brand-name">Converse</h2>
         </a>
@@ -50,7 +50,7 @@
     <div class="highlight-section sale-highlight">
       <div class="header-wrapper">
         <h1 class="highlight-header">Flash Sale</h1>
-        <a class="view-more-button" href="#">View More</a>
+        <a class="view-more-button" href="./catalog.php">View More</a>
       </div>
       <div class="bg-text bg-text-gray bg-text-top">Sale Sale Sale Sale Sale Sale Sale Sale Sale Sale</div>
       <div class="bg-text bg-text-gray bg-text-bottom">Sale Sale Sale Sale Sale Sale Sale Sale Sale Sale</div>
@@ -60,7 +60,7 @@
     <div class="highlight-section trending-highlight">
       <div class="header-wrapper">
         <h1 class="highlight-header">Trending</h1>
-        <a class="view-more-button" href="#">View More</a>
+        <a class="view-more-button" href="./catalog.php">View More</a>
       </div>
       <div class="bg-text bg-text-white bg-text-top">Trending Trending Trending Trending Trending Trending</div>
       <div class="bg-text bg-text-white bg-text-bottom">Trending Trending Trending Trending Trending Trending</div>
@@ -73,17 +73,12 @@
       </div>
       <div class="bg-text bg-text-gray bg-text-top">Explore Explore Explore Explore Explore Explore Explore</div>
       <div class="bg-text bg-text-gray bg-text-bottom">Explore Explore Explore Explore Explore Explore Explore</div>
-      <br />
-      <div class="category-products-container">
-        <div class="category-product-item">
-          <?php include "../components/product-card.php" ?>
-        </div>
-        <div class="category-product-item">
-          <?php include "../components/product-card.php" ?>
-        </div>
-        <div class="category-product-item">
-          <?php include "../components/product-card.php" ?>
-        </div>
+      <br /> <br />
+      <div class="category-carousel">
+        <a class="category-card sneakers-card" href="./catalog.php?category=Sneakers"><h3 class="category-name">Sneakers</h3></a>
+        <a class="category-card running-card" href="./catalog.php?category=Running"><h3 class="category-name">Running</h3></a>
+        <a class="category-card casual-card" href="./catalog.php?category=Casual"><h3 class="category-name">Casual</h3></a>
+        <a class="category-card below100-card" href="./catalog.php?min-price=0&max-price=100"><h3 class="category-name">Below $100</h3></a>
       </div>
     </div>
     <!-- Footer -->
