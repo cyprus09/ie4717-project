68b6590c72f24e1f95371d6ca068d6cff61d21a7
kevinlee3 <kevin.leegunawan8@gmail.com>
2024-11-02 01:34:53 +0800

    adjusted product card parameters

--- components/product-card.php
+++ components/product-card.php
@@ -5,3 +5,11 @@
         <p class="product-price">$<?php echo htmlspecialchars($product['price']); ?></p>
     </div>
 </a>
+
+<!-- <a class="product-card" href="../pages/prod-desc.php?name=<?php echo urlencode($product_name); ?>&price=<?php echo urlencode($product_price); ?>">
+    <div class="product-img"></div>
+    <div class="product-desc-section">
+        <h3 class="product-name"><?php echo htmlspecialchars($product_name); ?></h3>
+        <p class="product-price">$<?php echo htmlspecialchars($product_price); ?></p>
+    </div>
+</a> -->
