bf2116d2f556044024af83b8fc3e399fcb20236f
kevinlee3 <kevin.leegunawan8@gmail.com>
2024-11-04 01:02:58 +0800

    fixed product card text issue

--- components/product-card.php
+++ components/product-card.php
@@ -1,7 +1,12 @@
 <a class="product-card" href="../pages/prod-desc.php?name=<?php echo urlencode($product['name']); ?>&price=<?php echo urlencode($product['price']); ?>">
     <div class="product-img"></div>
     <div class="product-desc-section">
-        <h3 class="product-name"><?php echo htmlspecialchars($product['name']); ?></h3>
+        <h3 class="product-name">
+            <?php 
+                $name = htmlspecialchars($product['name']);
+                echo (strlen($name) > 35) ? substr($name, 0, 35) . '...' : $name;
+            ?>
+        </h3>
         <p class="product-price">$<?php echo htmlspecialchars($product['price']); ?></p>
     </div>
 </a>
