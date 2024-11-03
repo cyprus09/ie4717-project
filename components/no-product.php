a4201c5418574472eff9ba09d151a773b80d1504
kevinlee3 <kevin.leegunawan8@gmail.com>
2024-11-03 22:00:02 +0800

    fixed grammar and alignment

--- components/no-product.php
+++ components/no-product.php
@@ -1,4 +1,4 @@
 <div class="no-product">
     <img src="../assets/illustration/sorry.png">
-    <h2>Sorry, we can not <br> found the product</h2>
+    <h2>Sorry, we can not<br>find the product</h2>
 </div>
